ca6c4ecd24dbd810a0baa2faf3f670806578b2e8
cornel marian <marian_cornel_jr@yahoo.com>
2016-10-31 20:44:32 +0200

    StyleUrls

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -3,7 +3,8 @@ import { IProduct } from './products';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
 
 export class ProductListComponent {
