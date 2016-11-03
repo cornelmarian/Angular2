1b711c20794f51c78e32aa3f2534000b426511fc
cornel marian <marian_cornel_jr@yahoo.com>
2016-11-03 23:20:56 +0200

    Retrieving Data Using HTTP (Observables)

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -15,13 +15,14 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
     products: IProduct[];
 
     constructor(private _productService: ProductService) {
     }
 
     ngOnInit(): void {
-       this.products = this._productService.getProducts();
+        this._productService.getProducts().subscribe(products => this.products = products, error => this.errorMessage = <any>error);
     }
 
     toggleImage(): void {
